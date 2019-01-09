4442ae09d11e0e5ae33d63eea7b82c4e523982f5
Norbert Lam <norbertl@buffalo.edu>
2019-01-09 22:51:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,17 @@
 class Song
+  attr_reader @name, @artist, @genre
 
-  def initialize(name)
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@
+  end
+  
+  
 
 end
