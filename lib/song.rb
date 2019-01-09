42914fce9aed3594325a347fdf00e1788f2b698f
Norbert Lam <norbertl@buffalo.edu>
2019-01-09 23:01:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,19 @@ class Song
     return @@genres.uniq
   end
   
-  
+  def self.genre_count
+    genre_hash = {}
+    
+    @@genres.each do |genre|
+      if genre_hash.key?(genre)
+        genre_hash[genre] +=1
+      else
+        genre_hash[genre] = 1
+      end
+    end
+    
+    genre_hash
+  end
   
   
 
